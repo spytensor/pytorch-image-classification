117530ae40d6b64a7d6b9981b936e7d50dc9d661
spytensor <38940399+spytensor@users.noreply.github.com>
2018-09-06 18:48:14 +0800

    solved one bug

--- main.py
+++ main.py
@@ -20,11 +20,12 @@ import torch
 import time
 import os
 import warnings
+import json
 warnings.filterwarnings("ignore")
 os.environ["CUDA_VISIBLE_DEVICES"] = config.gpu
 
 #1.训练
-def train(train_loader, model, criterion, optimizer, epoch):
+def train(train_loader, model, criterion, optimizer, scheduler,epoch):
     batch_time = AverageMeter()
     data_time = AverageMeter()
     losses = AverageMeter()
@@ -34,6 +35,7 @@ def train(train_loader, model, criterion, optimizer, epoch):
         model.load_state_dict(torch.load(config.weights_path))
     model.train()
     end = time.time()
+    scheduler.step()
     for i, (images, target) in enumerate(tqdm(train_loader)):
         # measure data loading time
         data_time.update(time.time() - end)
@@ -41,7 +43,10 @@ def train(train_loader, model, criterion, optimizer, epoch):
         image_var = torch.autograd.Variable(images).cuda()
         label_var = torch.autograd.Variable(target).cuda()
         # compute y_pred
-        y_pred = model(image_var)
+        if config.model_name == "inception_v3":
+            y_pred,aux = model(image_var)
+        else:
+            y_pred = model(image_var)
         loss = criterion(y_pred, label_var)
         # measure accuracy and record loss
         prec1, prec1 = accuracy(y_pred.data, target, topk=(1, 1))
@@ -56,11 +61,9 @@ def train(train_loader, model, criterion, optimizer, epoch):
         end = time.time()
         if i % config.print_freq == 0:
             print('Epoch: [{0}][{1}/{2}]\t'
-                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
-                  'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
                   'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
-                  'Accuracy {acc.val:.3f} ({acc.avg:.3f})'.format(
-                      epoch, i, len(train_loader), batch_time=batch_time, data_time=data_time, loss=losses, acc=acc))
+                  'Accuracy [val: {acc.val:.3f}  avg: {acc.avg:.3f}]'.format(
+                      epoch, i, len(train_loader), loss=losses, acc=acc))
 #2.验证
 def val(val_loader, model, criterion):
     print("validating...")
@@ -86,62 +89,81 @@ def val(val_loader, model, criterion):
         end = time.time()
         if i % config.print_freq == 0:
             print('TrainVal: [{0}/{1}]\t'
-                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
                   'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
-                  'Accuracy {acc.val:.3f} ({acc.avg:.3f})'.format(
-                      i, len(val_loader), batch_time=batch_time, loss=losses, acc=acc))
+                  'Accuracy [val: {acc.val:.3f}  avg: {acc.avg:.3f}]'.format(
+                      i, len(val_loader), loss=losses, acc=acc))
     print(' * Accuracy {acc.avg:.3f}'.format(acc=acc))
     return acc.avg
 #3.测试
 def test(test_loader,model):
     model.load_state_dict(torch.load(config.weights_path))
     results = []
-    classes = {0:"first",1:"second",2:"third",3:"forth"}
-    with open("./results/test_result.txt","a") as f:
-        for i,(images,file_path) in enumerate(tqdm(test_loader)):
+    with open("./results/re.json","w",encoding="utf-8") as f:
+        iii = 0
+        for i,(images,file_paths) in enumerate(tqdm(test_loader)):
             image_var = torch.autograd.Variable(images).cuda()
-            y_pred = model(image_var)
-            prediction_index = torch.nn.functional.softmax(y_pred)[0].cpu().data.numpy().argmax()
-            f.write(file_path)
-            f.write("\t")
-            f.write(classes[prediction_index])
+            y_pred = model(image_var)            
+            for file_path,index in zip(file_paths,torch.nn.functional.softmax(y_pred)):
+                image_id = file_path.split("/")[-1]
+                disease_class = index.cpu().data.numpy().argmax()
+                results.append({"image_id":image_id,"disease_class":str(disease_class)})
+        json.dump(results,f,ensure_ascii=False)
 if __name__ == "__main__":
     #1.搭建模型
-    model = get_net(config.model_name)
+    model = torchvision.models.resnet152(pretrained=True)
+    for param in model.parameters():
+        param.requires_grad = False
+    model.fc = torch.nn.Linear(model.fc.in_features,config.num_classes)
     model.cuda()
     mode = "train"   
     #1.2定义评判标准和优化器
     optimizer = torch.optim.Adam(model.parameters(),lr=config.lr)
+    exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
     criterion = torch.nn.CrossEntropyLoss().cuda()
     #1.3数据增强方式
-    transforms = T.Compose([
+    transforms_train = T.Compose([
+        T.Resize((config.img_size,config.img_size)),
+        T.RandomRotation(90),
+        T.RandomHorizontalFlip(),
+        T.RandomVerticalFlip(),
+        T.RandomAffine(45),
+        T.ToTensor(),
+        T.Normalize(mean = [0.485,0.456,0.406],
+                    std = [0.229,0.224,0.225]),
+
+    ])
+    transforms_val_test = T.Compose([
         T.Resize((config.img_size,config.img_size)),
         T.ToTensor(),
         T.Normalize(mean = [0.485,0.456,0.406],
-                    std = [0.229,0.224,0.225])
+                    std = [0.229,0.224,0.225]),
+
     ])
     if mode == "train":
         #2.构建数据载入
-        train_data = cloth_data(config.data_path,transforms=transforms)
-        val_data = cloth_data(config.data_path,transforms=transforms,train=False)
+        train_data = cloth_data(config.data_path,transforms=transforms_train)
+        val_data = cloth_data(config.data_path,train=False,transforms=transforms_val_test)
         train_dataloader = torch.utils.data.DataLoader(train_data,batch_size=config.batch_size,shuffle=True)
         val_dataloader = torch.utils.data.DataLoader(val_data,batch_size=config.batch_size,shuffle=True)
-        # dataloaders = {"train":train_dataloader,
-        #                 "val":val_dataloader}
+
         #3.开启训练
         best_prec1 = 0
         for epoch in range(config.epochs):
-            train(train_dataloader,model,criterion,optimizer,epoch)
+            train(train_dataloader,model,criterion,optimizer,exp_lr_scheduler,epoch)
             prec1 = val(val_dataloader, model, criterion)
             is_best = prec1 > best_prec1
+            raw_score = best_prec1
             best_prec1 = max(prec1, best_prec1)
             if is_best :
-                print("Get Better model,the accuracy has updated to :",best_prec1.cpu().numpy())
+                try:
+                    print("Get Better model,the accuracy has updated from : {} to :{}".format(raw_score.cpu().numpy(),best_prec1.cpu().numpy()))
+                except:
+                    print("Get Better model,the accuracy has updated from : {} to :{}".format(raw_score,best_prec1.cpu().numpy()))
                 torch.save(model.state_dict(), config.weights_path)
             else:
                 print("No improvement in model performance ! Best accuracy is :",best_prec1.cpu().numpy())  
     else:
         print("testing!")
-        test_data = cloth_data(config.test_path,transforms=transforms,test=True)
+        test_data = cloth_data(config.test_path,transforms=transforms_val_test,test=True)
         test_loader = torch.utils.data.DataLoader(test_data,batch_size=config.batch_size,shuffle=True)
         test(test_loader,model)
